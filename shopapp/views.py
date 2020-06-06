f8eef53ec9370a63ed1fddb866f838f3e3354cf8
wahyudi <wahyudi.coding@gmail.com>
2020-06-06 14:05:25 +0700

    pagination

--- shopapp/views.py
+++ shopapp/views.py
@@ -18,6 +18,7 @@ class ProductsView(ListView):
     model = Product
     template_name = 'products/product_list.html'
     ordering = ['name']
+    paginate_by = 2
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
